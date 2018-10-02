1a1992f4d6ea9f3a842ac5dfb55e563e6c672728
aparnaperwala <39587393+aparnaperwala@users.noreply.github.com>
2018-10-02 15:23:03 -0700

    Add project files.

---
+++ Generics_LINQ_LamdaDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generics_LINQ_LamdaDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
